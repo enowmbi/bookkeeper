2dfb1907f54ca08e738828b7b6641b4ea86ea30c
Enow Mbi <benowmbi@yahoo.com>
2020-04-10 02:29:52 +0100

    Modify account.rb --add validation of name and code (for presence and uniqueness) --add validation for category to be included in CATEGORIES

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,13 @@
 class Account < ApplicationRecord
+
+  CATEGORIES = %w(Assets Liabilities Equity Revenue Expenses)
+
+  validates :name, presence: true, uniqueness: true
+  validates :code, presence: true, uniqueness: true
+  validates :category, presence: true, inclusion: {in: CATEGORIES}
+
+  def to_s
+    "#{code} - #{name}"
+  end
+
 end
